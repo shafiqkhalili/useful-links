f826e7aa7f11f724aca99bbee1fbf7fe3c1167aa
Shafigh Khalili <shafiqkhalili@gmail.com>
2020-09-12 16:02:40 +0200

    Add new file

---
+++ public/js/request.js
@@ -0,0 +1,25 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        requests: []
+    },
+    methods: {
+        upvoteRequest(id) {
+            const upvote = firebase.functions().httpsCallable('upvote');
+            upvote({ id }).catch(error => {
+                console.log(error.message);
+            });
+        }
+    },
+    mounted() {
+        const ref = firebase.firestore().collection('requests').orderBy('upvotes', 'desc');
+        ref.onSnapshot(snapshot => {
+            let requests = [];
+            snapshot.forEach(doc => {
+                requests.push({ ...doc.data(), id: doc.id });
+            });
+            this.requests = requests;
+
+        });
+    }
+});
